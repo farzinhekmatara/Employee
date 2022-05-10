497e17813996a150bcae8daf4370f221ad376a24
Lexicon Student 2 <Lexicon Student 2@DESKTOP-2GQ78GN>
2022-05-10 16:25:09 +0200

    Add project files.

---
+++ Employee/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Tankemässigt vet jag hur jag ska gå till väga. Problemet är mest hur man 
+ * kan uttrycka sig i denna miljö. jag saknar main funktionen och var programmet ska starta.
+ */
+
+
+
+using System;
+
+string name="";
+int salary=0;
+Console.WriteLine("Enter Name:");
+//Här vill jag läsa in från systeminput men vet inte hur man gör i C#.
+//name=Console.ReadLine()
+try
+{
+    name = Console.ReadLine();
+}
+catch (Exception)
+{
+
+}
+Console.WriteLine("Enter Salary");
+//Här vill jag läsa in från systeminput men vet inte hur man gör i C#.
+//salary=Console.ReadLine()
+try
+{
+    Console.ReadLine();
+}catch (Exception)
+{
+
+}
+Employee employee = new Employee(name,salary);
+Console.WriteLine("Name " + employee.firstName+ "salary " + employee.employeeSalary);
+
+public class Employee
+{
+    public string firstName;
+    public int employeeSalary;
+
+    public Employee(string name, int salary )
+    {
+        this.firstName = name;
+        this.employeeSalary = salary;
+    }
+
+}
+
